deda349f8e3f46a2b6f25ef1cc4e02e18639e47c
Farjana Epa <farjanaepa@gmail.com>
2025-09-22 16:36:34 +0600

    navbar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import navLogo from './assets/logo.png'
+import coin from './assets/images.png'
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="navbar">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">
+            <img className='w-[60px] h-[60px]' src={navLogo} alt="" />
+          </a>
+        </div>
+        <div className="flex items-center">
+         <span className='mr-1'>600000000000</span>
+         <span className='mr-1'> Coin </span>
+         <img className='w-[30px]' src={coin} alt="" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
